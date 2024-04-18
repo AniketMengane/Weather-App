7982dbc525cbb47fc6f49947c4a40d29372a2116
Aniket Mengane <157455784+AniketMengane@users.noreply.github.com>
2024-04-18 12:35:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Wetherapp from './Components/WetherApp/wetherapp';
+
+function App() {
+  return (
+    <div className="App">
+       <Wetherapp/>
+    </div>
+  );
+}
+
+export default App;
